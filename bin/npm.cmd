e2da8f73354669e7a51e0a981306fed0da2bde86
DeeDeeG <DeeDeeG@users.noreply.github.com>
2020-07-22 15:18:50 -0400

    bin: Find bundled npm when apm is a dependency

--- bin/npm.cmd
+++ bin/npm.cmd
@@ -6,4 +6,14 @@ set "PATH=%~dp0;%PATH%"
 :: Force npm to use its builtin node-gyp
 set npm_config_node_gyp=
 
-"%~dp0\..\node_modules\.bin\npm.cmd" %*
+:: Find bundled npm under this package's node_modules,
+:: or in a node_modules one level above this package
+:: (for when apm is a dependency)
+if exist "%~dp0\..\node_modules\.bin\npm.cmd" (
+    "%~dp0\..\node_modules\.bin\npm.cmd" %*
+) else if exist "%~dp0\..\..\.bin\npm.cmd" (
+    "%~dp0\..\..\.bin\npm.cmd" %*
+) else (
+    rem "error: apm couldn't find its bundled npm."
+    exit /B 2
+)
