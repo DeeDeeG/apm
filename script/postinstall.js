2830598d545774dbbf4f2a49d02fb0079c1de58a
DeeDeeG <DeeDeeG@users.noreply.github.com>
2020-08-14 18:06:15 -0400

    script/postinstall.js: No cmd or bash stuff.

--- script/postinstall.js
+++ script/postinstall.js
@@ -11,6 +11,14 @@ if (process.platform === 'win32') {
   script += '.sh'
 }
 
+if (process.platform === 'win32') {
+  npmWrapperExtension = '.cmd'
+} else {
+  npmWrapperExtension += ''
+}
+
+const npmWrapperPath = path.join(__dirname, '..', 'bin', 'npm' + npmWrapperExtension)
+
 // Make sure all the scripts have the necessary permissions when we execute them
 // (npm does not preserve permissions when publishing packages on Windows,
 // so this is especially needed to allow apm to be published successfully on Windows)
@@ -18,6 +26,30 @@ fs.chmodSync(script, 0o755)
 fs.chmodSync(path.join(__dirname, '..', 'bin', 'apm'), 0o755)
 fs.chmodSync(path.join(__dirname, '..', 'bin', 'npm'), 0o755)
 
-var child = cp.spawn(script, [], { stdio: ['pipe', 'pipe', 'pipe'], shell: true })
-child.stderr.pipe(process.stderr)
-child.stdout.pipe(process.stdout)
+// var child = cp.spawn(script, [], { stdio: ['pipe', 'pipe', 'pipe'], shell: true })
+// child.stderr.pipe(process.stderr)
+// child.stdout.pipe(process.stdout)
+
+console.log('>> Downloading bundled Node')
+cp.spawnSync(
+  'node',
+  [path.join(__dirname, 'download-node.js')]
+)
+
+const downloadedNodeVersionAndArch = cp.execFileSync('./bin/node', ['-p', 'process.version + " " + process.arch'], { encoding: 'utf8' })
+
+console.log('\n>> Rebuilding apm dependencies with bundled Node ' + downloadedNodeVersionAndArch)
+cp.execFileSync(
+  npmWrapperPath,
+  ['rebuild']
+)
+
+if (process.env.NO_APM_DEDUPE && process.env.NO_APM_DEDUPE.length > 0) {
+  console.log('\n>> Deduplication disabled')
+} else {
+  console.log('\n>> Deduping apm dependencies')
+  cp.execFileSync(
+    npmWrapperPath,
+    ['dedupe']
+  )
+}
