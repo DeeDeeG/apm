07db8289bdf60061cddd1cb7e0f1bb784e84c225
DeeDeeG <DeeDeeG@users.noreply.github.com>
2021-01-13 02:43:58 -0500

    bin\npm.cmd: Find npm when not installed with --global-style

--- bin/npm.cmd
+++ bin/npm.cmd
@@ -6,4 +6,16 @@ set "PATH=%~dp0;%PATH%"
 :: Force npm to use its builtin node-gyp
 set npm_config_node_gyp=
 
-"%~dp0\..\node_modules\.bin\npm.cmd" %*
+echo Looking for npm at "%~dp0..\node_modules\.bin\npm.cmd"
+if exist "%~dp0..\node_modules\.bin\npm.cmd" (
+  echo Found npm at "%~dp0..\node_modules\.bin\npm.cmd"
+  "%~dp0..\node_modules\.bin\npm.cmd" %*
+) else (
+  echo Looking for npm at "%~dp0..\..\..\node_modules\.bin\npm.cmd"
+  if exist "%~dp0..\..\..\node_modules\.bin\npm.cmd" (
+    echo Found npm at "%~dp0..\..\..\node_modules\.bin\npm.cmd"
+    "%~dp0..\..\..\node_modules\.bin\npm.cmd" %*
+  ) else (
+    echo Couldn't find npm
+  )
+)
