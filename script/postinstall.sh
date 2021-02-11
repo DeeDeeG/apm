eaed7035aacd5e12aa1172a87efbf50ea7bc4209
DeeDeeG <DeeDeeG@users.noreply.github.com>
2021-02-10 21:21:00 -0500

    script/postinstall.sh: Fix endless loop with npm 7
    `npm rebuild` started running the postinstall script after completing.
    This calls npm rebuild again, triggering the postinstall script again,
    in an endless, recursive loop.
    
    So set an environment variable "APM_ALREADY_REBUILDING"
    before doing npm rebuild the first time,
    and exit the postinstall script early if that is set,
    and the endless loop will be broken.

--- script/postinstall.sh
+++ script/postinstall.sh
@@ -2,11 +2,19 @@
 
 set -e
 
+# Skip the postinstall script if we're already rebuilding,
+# to avoid an endless, recursive postinstall loop.
+if [ -n "${APM_ALREADY_REBUILDING}" ]; then
+  echo ">> Postinstall script is already being run. Skipping recursive call."
+  exit 0
+fi
+
 echo ">> Downloading bundled Node"
 node script/download-node.js
 
 echo
 echo ">> Rebuilding apm dependencies with bundled Node $(./bin/node -p "process.version + ' ' + process.arch")"
+export APM_ALREADY_REBUILDING="true"
 ./bin/npm rebuild
 
 echo
