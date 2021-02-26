853abef610739f9a1bd9e1976b388ddb29de7b34
DeeDeeG <DeeDeeG@users.noreply.github.com>
2021-02-26 16:14:24 -0500

    postinstall.cmd: Avoid infinite rebuild loop

--- script/postinstall.cmd
+++ script/postinstall.cmd
@@ -2,12 +2,21 @@
 setlocal EnableDelayedExpansion
 setlocal EnableExtensions
 
+:: Skip the postinstall script if we're already rebuilding,
+:: to avoid an endless, recursive postinstall loop.
+if defined APM_ALREADY_REBUILDING (
+  echo.
+  echo ^>^> Postinstall script is already being run. Skipping recursive call.
+  exit /b 0
+)
+
 echo ^>^> Downloading bundled Node
 node .\script\download-node.js
 
 echo.
 for /f "delims=" %%i in ('.\bin\node.exe -p "process.version + ' ' + process.arch"') do set bundledVersion=%%i
 echo ^>^> Rebuilding apm dependencies with bundled Node !bundledVersion!
+set APM_ALREADY_REBUILDING=true
 call .\bin\npm.cmd rebuild
 
 if defined NO_APM_DEDUPE (
