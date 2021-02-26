deca752074796b7ecafa89fb624bb82e96ba3be8
DeeDeeG <DeeDeeG@users.noreply.github.com>
2021-02-26 18:31:46 -0500

    script/postinstall.js: Use recommended way to skip parts of a Node.JS script
    exit() is synchronous. It may cut off asynchronous operations.
    "Avoiding scheduling more work" (by putting it behind an if(){})
    is the recommended way to skip parts of the script.

--- script/postinstall.js
+++ script/postinstall.js
@@ -1,29 +1,32 @@
 #!/usr/bin/env node
 
-var cp = require('child_process')
-var fs = require('fs')
-var path = require('path')
+if (process.env.APM_ALREADY_RUNNING_POSTINSTALL !== 'true') {
 
-if (process.env.APM_ALREADY_RUNNING_POSTINSTALL === 'true') {
-  console.log('\n>> Postinstall script is already being run. Skipping recursive call.\n')
-  process.exit(0)
-}
+  var cp = require('child_process')
+  var fs = require('fs')
+  var path = require('path')
 
-var script = path.join(__dirname, 'postinstall')
-if (process.platform === 'win32') {
-  script += '.cmd'
+  var script = path.join(__dirname, 'postinstall')
+  if (process.platform === 'win32') {
+    script += '.cmd'
+  } else {
+    script += '.sh'
+  }
+
+  // Make sure all the scripts have the necessary permissions when we execute them
+  // (npm does not preserve permissions when publishing packages on Windows,
+  // so this is especially needed to allow apm to be published successfully on Windows)
+  fs.chmodSync(script, 0o755)
+  fs.chmodSync(path.join(__dirname, '..', 'bin', 'apm'), 0o755)
+  fs.chmodSync(path.join(__dirname, '..', 'bin', 'npm'), 0o755)
+
+  process.env.APM_ALREADY_RUNNING_POSTINSTALL = 'true'
+  var child = cp.spawn(script, [], { stdio: ['pipe', 'pipe', 'pipe'], shell: true })
+  child.stderr.pipe(process.stderr)
+  child.stdout.pipe(process.stdout)
+
 } else {
-  script += '.sh'
+
+  console.log('\n>> Postinstall script is already being run. Skipping recursive call.\n')
+
 }
-
-// Make sure all the scripts have the necessary permissions when we execute them
-// (npm does not preserve permissions when publishing packages on Windows,
-// so this is especially needed to allow apm to be published successfully on Windows)
-fs.chmodSync(script, 0o755)
-fs.chmodSync(path.join(__dirname, '..', 'bin', 'apm'), 0o755)
-fs.chmodSync(path.join(__dirname, '..', 'bin', 'npm'), 0o755)
-
-process.env.APM_ALREADY_RUNNING_POSTINSTALL = 'true'
-var child = cp.spawn(script, [], { stdio: ['pipe', 'pipe', 'pipe'], shell: true })
-child.stderr.pipe(process.stderr)
-child.stdout.pipe(process.stdout)
